beam: allow no out in change_op gradient calc/funcs/examples.rec: fix mse related functions

examples/simple_reconstruction_batch.py
@@ -6,33 +6,35 @@ from pycuda import gpuarray
 import ssnp
 from ssnp import BeamArray, calc
 
-ssnp.config.res = (0.1, 0.1, 0.1)
 n = ssnp.read("bb.tiff", dtype=np.double, gpu=True)
-n *= 0.
+n.fill(0)  # zero initialization
 ng = gpuarray.empty_like(n)
 
+mea = ssnp.read("meabb.tiff", np.double, gpu=True)
+mea *= 2  # restore data (divided by 2 before save)
+
+ssnp.config.res = (0.1, 0.1, 0.1)
 NA = 0.65
+STEPS = 5
+STEP_SIZE = 350
+ANGLE_NUM = len(mea)
 
 u_list = []
 u_plane = ssnp.read("plane", np.complex128, shape=n.shape[1:], gpu=False)
-u_plane = np.broadcast_to(u_plane, (8, *u_plane.shape)).copy()
+u_plane = np.broadcast_to(u_plane, (ANGLE_NUM, *u_plane.shape)).copy()
 beam = BeamArray(u_plane, total_ops=len(n))
 beam_in = BeamArray(u_plane, total_ops=len(n))
 
-pupil = beam.multiplier.binary_pupil(0.6501, gpu=True)
+pupil = beam.multiplier.binary_pupil(NA * 1.001, gpu=True)
 
-for num in range(8):
-    xy_theta = num / 8 * 2 * np.pi
+for num in range(ANGLE_NUM):
+    xy_theta = num / ANGLE_NUM * 2 * np.pi
     c_ab = NA * np.cos(xy_theta), NA * np.sin(xy_theta)
     beam_in.forward[num] *= beam.multiplier.tilt(c_ab, trunc=True, gpu=True)
 
-mea = ssnp.read("meabb.tiff", np.double, gpu=True)
-mea *= 2
-
 t = time()
-for step in range(5):
+for step in range(STEPS):
     print(f"Step: {step}")
-    # ng_total *= 0
     beam.forward = beam_in.forward
     beam.backward = 0
     beam.merge_prop()
@@ -42,12 +44,12 @@ for step in range(5):
         beam.a_mul(pupil)
         loss = beam.forward_mse_loss(mea)
     print(f"{loss = :f}")
-    loss = [calc.reduce_mse(beam.forward[i], mea[i]) for i in range(8)]
-    print(f"loss detail: {', '.join([f'{i:6.1f}' for i in loss])}")
+    loss = [calc.reduce_mse(beam.forward[i], mi) for i, mi in enumerate(mea)]
+    print(f"loss detail: {', '.join([f'{i:.2e}' for i in loss])}")
     beam.n_grad(ng)
-    ng *= 0.005
+    ng *= STEP_SIZE
     n -= ng
-    n = gpuarray.maximum(n, 0, out=n)
+    n = gpuarray.maximum(n, 0, out=n)  # positive regularization
 print(time() - t)
 
 ssnp.write("ssnp_recbb.tiff", n)

ssnp_pkg/src/ssnp/calc.py
@@ -90,11 +90,11 @@ def reduce_mse(u, measurement, stream=None):
     if u.dtype != np.complex128:
         raise ValueError(f"u dtype {u.dtype} is incompatible")
     funcs = get_funcs(u, model="any", stream=stream)
-    result = funcs.reduce_mse(u, measurement)
+    result = funcs.reduce_sse(u, measurement)
     if stream is None:
-        return result.get()
+        return result.get() / u.size
     else:
-        return result.get(pagelocked=True, async_=True, stream=stream)
+        return result.get(pagelocked=True, async_=True, stream=stream) / u.size
 
 
 def reduce_mse_grad(u, measurement, output=None, stream=None):

ssnp_pkg/src/ssnp/funcs.py
@@ -12,18 +12,18 @@ import logging
 class Funcs:
     # __temp_memory_pool = {}
     _funcs_cache = {}
-    reduce_mse_cr_krn = None
+    reduce_sse_cr_krn = None
 
     def __new__(cls, arr_like, res, n0, stream=None, fft_type="reikna"):
-        if cls.reduce_mse_cr_krn is None:
-            Funcs.reduce_mse_cr_krn = reduction.ReductionKernel(
+        if cls.reduce_sse_cr_krn is None:
+            Funcs.reduce_sse_cr_krn = reduction.ReductionKernel(
                 dtype_out=np.double, neutral=0,
                 reduce_expr="a+b",
                 map_expr="(cuCabs(x[i]) - y[i]) * (cuCabs(x[i]) - y[i])",
                 arguments="double2 *x, double *y",
                 preamble='#include "cuComplex.h"'
             )
-            Funcs.reduce_mse_cc_krn = reduction.ReductionKernel(
+            Funcs.reduce_sse_cc_krn = reduction.ReductionKernel(
                 dtype_out=np.double, neutral=0,
                 reduce_expr="a+b",
                 map_expr="cuCabs(cuCsub(x[i], y[i])) * cuCabs(cuCsub(x[i], y[i]))",
@@ -34,7 +34,7 @@ class Funcs:
                 "double2 *u, double2 *m, double2 *out",
                 """
                 out[i] = cuCsub(u[i], m[i]);
-                out[i].x *= 2; out[i].y *= 2;
+                out[i].x *= 2. / (double)n; out[i].y *= 2. / (double)n;
                 """,
                 preamble='#include "cuComplex.h"'
             )
@@ -42,7 +42,7 @@ class Funcs:
                 "double2 *u, double *m, double2 *out",
                 """
                 temp = 2 * (1 - m[i] / cuCabs(u[i]));
-                out[i].x = temp * u[i].x; out[i].y = temp * u[i].y;
+                out[i].x = temp * u[i].x / (double)n; out[i].y = temp * u[i].y / (double)n;
                 """,
                 loop_prep="double temp",
                 preamble='#include "cuComplex.h"'
@@ -131,10 +131,6 @@ class Funcs:
             """,
             loop_prep="unsigned j",
         )
-        # self.reduce_mse_cr = partial(self.reduce_mse_cr_krn, stream=stream)
-        # self.reduce_mse_cc = partial(self.reduce_mse_cc_krn, stream=stream)
-        # self.mse_cc_grad = partial(self.mse_cc_grad_krn, stream=stream)
-        # self.mse_cr_grad = partial(self.mse_cr_grad_krn, stream=stream)
 
     def _initialized(self):
         return self._funcs_cache is None
@@ -162,12 +158,12 @@ class Funcs:
                                  out.gpudata, x.mem_size)
         return out
 
-    def reduce_mse(self, field, measurement):
+    def reduce_sse(self, field, measurement):
         if field.dtype == np.complex128:
             if measurement.dtype == np.float64:
-                return Funcs.reduce_mse_cr_krn(field, measurement, stream=self.stream)
+                return Funcs.reduce_sse_cr_krn(field, measurement, stream=self.stream)
             elif measurement.dtype == np.complex128:
-                return Funcs.reduce_mse_cc_krn(field, measurement, stream=self.stream)
+                return Funcs.reduce_sse_cc_krn(field, measurement, stream=self.stream)
         raise TypeError(f"incompatible dtype: field {field.dtype}, measurement {measurement.dtype}")
 
     def mse_grad(self, field, measurement, gradient):